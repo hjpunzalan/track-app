b8e601fd912f426e74e9d1ea26742e0fea7b159b
hjpunzalan <hj.punzalan@hotmail.com>
2020-05-27 17:34:35 +0800

    Set up all six screens boilerplate

---
+++ client/src/screens/SignupScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SignupScreen = () => {
+	return (
+		<View>
+			<Text style={{ fontSize: 48 }}>Sign up</Text>
+		</View>
+	);
+};
+
+export default SignupScreen;
+
+const styles = StyleSheet.create({});
