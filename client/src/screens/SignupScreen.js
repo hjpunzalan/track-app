9b5ee57566bc1093a4dfefcfb42bcc928fd87703
hjpunzalan <hj.punzalan@hotmail.com>
2020-05-28 11:44:16 +0800

    Styling sign up screen

--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
 	return (
-		<>
+		<View style={styles.container}>
 			<Spacer>
 				<Text style={{ fontSize: 48 }}>Sign up</Text>
 			</Spacer>
@@ -15,10 +15,21 @@ const SignupScreen = ({ navigation }) => {
 			<Spacer>
 				<Button title="Sign up" />
 			</Spacer>
-		</>
+		</View>
 	);
 };
 
+SignupScreen.navigationOptions = () => {
+	return {
+		headerShown: false,
+	};
+};
 export default SignupScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		marginBottom: "25%",
+	},
+});
