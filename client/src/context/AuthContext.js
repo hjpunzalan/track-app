4e7bcc9e68b2e521368bd8725cad040c5413aa60
hjpunzalan <hj.punzalan@hotmail.com>
2020-05-28 12:26:06 +0800

    Auth context actions

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -7,6 +7,23 @@ const authReducer = (state, action) => {
 	}
 };
 
+// Actions
+const signup = (dispatch) => ({ email, password }) => {
+	// make api request to sign up with that email and password
+	// once sign up, modify state and authenticate
+	// if sign up fail, need to reflect an error message
+};
+
+const signin = (dispatch) => ({ email, password }) => {
+	// Try to sign in
+	// Handle success by updating state
+	// Handle failure by showing error message
+};
+
+const signout = (dispatch) => () => {
+	// simply sign out changing authentication
+};
+
 export const { Provider, Context } = createDataContext(
 	authReducer,
 	{},
