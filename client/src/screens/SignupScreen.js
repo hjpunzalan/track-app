fa2bab79729a642488a4f7a383a30b320236044d
hjpunzalan <hj.punzalan@hotmail.com>
2020-05-31 23:58:08 +0800

    Reusable AuthForm component for signin and signup

--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -1,41 +1,28 @@
 import React, { useState, useContext } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
 
 const SignupScreen = ({ navigation }) => {
 	const { state, signup } = useContext(AuthContext);
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
 
 	return (
 		<View style={styles.container}>
-			<Spacer>
-				<Text style={{ fontSize: 48 }}>Sign up</Text>
-			</Spacer>
-			<Input
-				label="Email"
-				value={email}
-				onChangeText={setEmail}
-				autoCapitalize="none"
-				autoCorrect={false}
+			<AuthForm
+				headerText="Sign Up for Tracker"
+				errorMessage={state.errorMessage}
+				submitButtonText="Sign Up"
+				onSubmit={signup} //({ email, password }) => signup({ email, password })
 			/>
-			<Spacer />
-			<Input
-				label="Password"
-				value={password}
-				onChangeText={setPassword}
-				autoCapitalize="none"
-				autoCorrect={false}
-				secureTextEntry={true}
-			/>
-			{state.errorMessage ? (
-				<Text style={styles.errorMessage}>{state.errorMessage}</Text>
-			) : null}
-			<Spacer>
-				<Button title="Sign up" onPress={() => signup({ email, password })} />
-			</Spacer>
+			<TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+				<Spacer>
+					<Text style={styles.link}>
+						Already have an account? Sign in instead
+					</Text>
+				</Spacer>
+			</TouchableOpacity>
 		</View>
 	);
 };
@@ -51,12 +38,9 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: "center",
-		marginBottom: "25%",
+		marginBottom: "10%",
 	},
-	errorMessage: {
-		fontSize: 16,
-		color: "red",
-		marginLeft: 15,
-		marginTop: 15,
+	link: {
+		color: "blue",
 	},
 });
