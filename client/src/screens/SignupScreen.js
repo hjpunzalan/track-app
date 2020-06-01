6b5110b30c6f13b1965dca94412b3f6bd1a3ab7f
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-01 19:17:59 +0800

    create NavLink reusable component for signin and signup

--- client/src/screens/SignupScreen.js
+++ client/src/screens/SignupScreen.js
@@ -1,11 +1,10 @@
-import React, { useState, useContext } from "react";
-import { StyleSheet, View, TouchableOpacity } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
-import Spacer from "../components/Spacer";
+import React, { useContext } from "react";
+import { StyleSheet, View } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
-const SignupScreen = ({ navigation }) => {
+const SignupScreen = () => {
 	const { state, signup } = useContext(AuthContext);
 
 	return (
@@ -16,13 +15,10 @@ const SignupScreen = ({ navigation }) => {
 				submitButtonText="Sign Up"
 				onSubmit={signup} //({ email, password }) => signup({ email, password })
 			/>
-			<TouchableOpacity onPress={() => navigation.navigate("Signin")}>
-				<Spacer>
-					<Text style={styles.link}>
-						Already have an account? Sign in instead
-					</Text>
-				</Spacer>
-			</TouchableOpacity>
+			<NavLink
+				routeName="Signin"
+				text="Already have an account? Sign in instead"
+			/>
 		</View>
 	);
 };
@@ -39,8 +35,5 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		marginBottom: "10%",
-	},
-	link: {
-		color: "blue",
 	},
 });
