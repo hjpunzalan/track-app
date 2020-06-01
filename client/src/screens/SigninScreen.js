b00aefaf56e4a04ca31d564231d1fb53dd7a95a4
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-01 19:39:38 +0800

    Apply reusable components to signin component

--- client/src/screens/SigninScreen.js
+++ client/src/screens/SigninScreen.js
@@ -1,14 +1,39 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SigninScreen = () => {
+	const { state, signin } = useContext(AuthContext);
 	return (
-		<View>
-			<Text style={{ fontSize: 48 }}>Sign in</Text>
+		<View style={styles.container}>
+			<AuthForm
+				headerText="Sign in to your Account"
+				errorMessage={state.errorMessage}
+				onSubmit={signin}
+				submitButtonText="Sign In"
+			/>
+			<NavLink
+				text="Don't have an account? Sign up instead"
+				routeName="Signup"
+			/>
 		</View>
 	);
 };
 
+SigninScreen.navigationOptions = () => {
+	return {
+		headerShown: false,
+	};
+};
+
 export default SigninScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		marginBottom: "10%",
+	},
+});
