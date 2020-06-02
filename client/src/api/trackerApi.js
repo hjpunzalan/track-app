2715e139cf51f9d4313d817188cdb103eeb831d2
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-02 12:33:35 +0800

    New auth screen to resolve if user is logged in

--- client/src/api/trackerApi.js
+++ client/src/api/trackerApi.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
 	// To be changed every 8hrs
-	baseURL: "http://fb710015ece3.ngrok.io",
+	baseURL: "http://localhost:3000",
 });
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -10,12 +10,28 @@ const authReducer = (state, action) => {
 		case "signup":
 		case "signin":
 			return { token: action.payload, errorMessage: "" };
+		case "clear_error_message":
+			return { ...state, errorMessage: "" };
 		default:
 			return state;
 	}
 };
 
 // Actions
+const tryLocalSignin = (dispatch) => async () => {
+	const token = await AsyncStorage.getItem("token");
+	if (token) {
+		dispatch({ type: "signin", payload: token });
+		navigate("TrackList");
+	} else {
+		navigate("loginFlow");
+	}
+};
+
+const clearErrorMessage = (dispatch) => () => {
+	dispatch({ type: "clear_error_message" });
+};
+
 const signup = (dispatch) => async ({ email, password }) => {
 	// make api request to sign up with that email and password
 	try {
@@ -55,6 +71,6 @@ const signout = (dispatch) => () => {
 
 export const { Provider, Context } = createDataContext(
 	authReducer,
-	{ signup, signin, signout },
+	{ signup, signin, signout, clearErrorMessage, tryLocalSignin },
 	{ token: null, errorMessage: "" }
 );
