8e56f59111e3d044332ebcfde1551049c6638af8
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-02 21:25:02 +0800

    Drawing series of points with poly line and mapview

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,14 +1,40 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+	let points = [];
+	for (let i = 0; i < 10; i++) {
+		if (i % 2 === 0) {
+			points.push({
+				latitude: -32.00727 + i * 0.001,
+				longitude: 115.938969 + i * 0.001,
+			});
+		} else {
+			points.push({
+				latitude: -32.00727 - i * 0.002,
+				longitude: 115.938969 - i * 0.001,
+			});
+		}
+	}
 	return (
-		<View>
-			<Text>I am a map</Text>
-		</View>
+		<MapView
+			style={styles.map}
+			initialRegion={{
+				latitude: -32.00727,
+				longitude: 115.938969,
+				latitudeDelta: 0.01,
+				longitudeDelta: 0.01,
+			}}>
+			<Polyline coordinates={points} />
+		</MapView>
 	);
 };
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	map: {
+		height: 300,
+	},
+});
