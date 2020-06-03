d5394d9aead567af8b9e38898632dd2b5b58d25e
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-03 22:11:12 +0800

    LocationContext added to App.js

--- client/App.js
+++ client/App.js
@@ -9,6 +9,7 @@ import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
 import { Provider as AuthProvider } from "./src/context/AuthContext";
+import { Provider as LocationProvider } from "./src/context/LocationContext";
 import { setNavigator } from "./src/navigationRef";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 
@@ -32,6 +33,8 @@ const App = createAppContainer(switchNavigator);
 
 export default () => (
 	<AuthProvider>
-		<App ref={(navigator) => setNavigator(navigator)} />
+		<LocationProvider>
+			<App ref={(navigator) => setNavigator(navigator)} />
+		</LocationProvider>
 	</AuthProvider>
 );
