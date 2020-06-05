9a7860f03219f9e08da362a03cbd4d29cd9d00cf
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-05 10:05:54 +0800

    extract useLocation hook

---
+++ client/src/hooks/useLocation.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import {
+	Accuracy,
+	requestPermissionsAsync,
+	watchPositionAsync,
+} from "expo-location";
+
+export default (callback) => {
+	const [err, setErr] = useState(null);
+
+	const startWatching = async () => {
+		try {
+			const { granted } = await requestPermissionsAsync();
+			if (!granted) throw new Error("Location permission not granted");
+
+			// Watch user position
+			await watchPositionAsync(
+				{
+					accuracy: Accuracy.BestForNavigation,
+					timeInterval: 1000,
+					distance: 10,
+				},
+				(location) => {
+					callback(location);
+				}
+			);
+		} catch (err) {
+			setErr(err.message);
+		}
+	};
+
+	useEffect(() => {
+		startWatching();
+	}, []);
+
+	return [err];
+};
