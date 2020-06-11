ff2b6ceeb40c1985450f038a2ed59f4150e5fdc2
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-11 21:14:11 +0800

    Navigating to a saved track

--- client/src/screens/TrackListScreen.js
+++ client/src/screens/TrackListScreen.js
@@ -1,15 +1,35 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, Text, FlatList, TouchableOpacity } from "react-native";
+import { ListItem } from "react-native-elements";
+import { NavigationEvents } from "react-navigation";
+import { Context as TrackContext } from "../context/TrackContext";
+import Spacer from "../components/Spacer";
 
 const TrackListScreen = ({ navigation }) => {
+	const { state, fetchTracks } = useContext(TrackContext);
 	return (
-		<View>
+		<>
+			<NavigationEvents onWillFocus={fetchTracks} />
 			<Text style={{ fontSize: 48 }}>Track List</Text>
-			<Button
-				title="Go to track detail"
-				onPress={() => navigation.navigate("TrackDetail")}
-			/>
-		</View>
+			<Spacer>
+				<FlatList
+					data={state}
+					keyExtractor={(item) => item._id}
+					renderItem={({ item }) => {
+						return (
+							<TouchableOpacity
+								onPress={() =>
+									navigation.navigate("TrackDetail", {
+										_id: item._id,
+									})
+								}>
+								<ListItem chevron title={item.name} />
+							</TouchableOpacity>
+						);
+					}}
+				/>
+			</Spacer>
+		</>
 	);
 };
 
