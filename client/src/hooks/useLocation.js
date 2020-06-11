2a875be21902846134af113bb8a566fa0c133ffa
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-11 11:28:28 +0800

    Avoiding stale references with cleaner code

--- client/src/hooks/useLocation.js
+++ client/src/hooks/useLocation.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
 	Accuracy,
 	requestPermissionsAsync,
@@ -7,27 +7,6 @@ import {
 
 export default (shouldTrack, callback) => {
 	const [err, setErr] = useState(null);
-	const [subscriber, setSubscriber] = useState(null);
-
-	const startWatching = async () => {
-		try {
-			const { granted } = await requestPermissionsAsync();
-			if (!granted) throw new Error("Location permission not granted");
-
-			// Watch user position
-			const sub = await watchPositionAsync(
-				{
-					accuracy: Accuracy.BestForNavigation,
-					timeInterval: 1000,
-					distance: 10,
-				},
-				callback
-			);
-			setSubscriber(sub);
-		} catch (err) {
-			setErr(err.message);
-		}
-	};
 
 	// Cant add callback to dependency of useEffect because a new location will trigger an update of callback and startWatching,
 	//  everytime theeres a new location thus breaking the app
@@ -36,11 +15,31 @@ export default (shouldTrack, callback) => {
 	// useCallback does not send a new callback if state.recording is the same
 
 	useEffect(() => {
+		let subscriber;
+		const startWatching = async () => {
+			try {
+				const { granted } = await requestPermissionsAsync();
+				if (!granted) throw new Error("Location permission not granted");
+
+				// Watch user position
+				subscriber = await watchPositionAsync(
+					{
+						accuracy: Accuracy.BestForNavigation,
+						timeInterval: 1000,
+						distance: 10,
+					},
+					callback
+				);
+			} catch (err) {
+				setErr(err.message);
+			}
+		};
+
 		if (shouldTrack) {
 			startWatching();
 		} else {
-			subscriber.remove();
-			setSubscriber(null);
+			if (subscriber) subscriber.remove();
+			subscriber = null;
 		}
 		// Prevents multiple startwatching calls
 		return () => {
@@ -50,3 +49,6 @@ export default (shouldTrack, callback) => {
 
 	return [err];
 };
+
+// Any reference to state,context,prop values to the useEffect hook must be added to the dependency list!!!!!
+// TRY NOT TO define helper functions that reference props, state, context outside of useEffect!!
